fa2bc74d9047819fb9bc09b2250111d6f0bbad75
jubeaz <pascal.auillans@gmail.com>
2024-04-20 16:56:59 +0200

    update

--- providers/scripts/PowerAction.ps1
+++ providers/scripts/PowerAction.ps1
@@ -6,14 +6,58 @@ try {
     powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 3
 }
 catch {
-    Write-Host "An error occurred: continuing"
+    Write-Host "An error occurred while setting shutdown on powerbutton"
 }
-Set-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\policies\system" -Name "shutdownwithoutlogon" -Value 1
-try {
-    Set-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\PolicyManager\default\Start\HideRestart" -Name "value" -Value 0 -ErrorAction Stop
-    Set-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\PolicyManager\default\Start\HideShutDown" -Name "value" -Value 0 -ErrorAction Stop
-    New-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\windows" -Name "ShutdownWarningDialoTimeout" -ProperyType DWord -value 1
+
+# Enabling immediate shutdown of a virtual server with logged in users.
+if ((Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\windows").PSObject.Properties.Name -contains "ShutdownWarningDialoTimeout") {
+    try {
+        Set-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\windows" -Name "ShutdownWarningDialoTimeout" -value 1 
+    } catch {
+        Write-Host "An error occurred while trying to Enabling immediate shutdown of a virtual server with logged in users." 
+    }
+} else {
+    try {
+        New-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\windows" -Name "ShutdownWarningDialoTimeout" -value 1
+    } catch {
+        Write-Host "An error occurred while trying to (create) Enabling immediate shutdown of a virtual server with logged in users." 
+    }
 }
-catch {
-    Write-Host "An error occurred: continuing"
+
+# Allow system to be shut down without having to log on
+if ((Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\policies\system").PSObject.Properties.Name -contains  "shutdownwithoutlogon") {
+    try {
+        Set-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\policies\system" -Name "shutdownwithoutlogon" -value 1 
+    } catch {
+        Write-Host "An error occurred while trying to Allow system to be shut down without having to log on" 
+    }
+} else {
+    try {
+        New-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\policies\system" -Name "shutdownwithoutlogon" -value 1
+    } catch {
+        Write-Host "An error occurred while trying to (create) Allow system to be shut down without having to log on" 
+    }
+}
+
+
+# Enabling Default Reply
+if (-not (Test-Path -path "HKLM:SYSTEM\CurrenctControlSet\Control\Error Message Instrument")) {
+    try {
+        New-Item -Path "HKLM:SYSTEM\CurrenctControlSet\Control" -Name "Error Message Instrument" -Force 
+    } catch {
+        Write-Host "An error during creation : HKLM:SYSTEM\CurrenctControlSet\Control\Error Message Instrument" 
+    }
+} 
+if ((Get-ItemProperty -Path "HKLM:SYSTEM\CurrenctControlSet\Control\Error Message Instrument").PSObject.Properties.Name -contains  "EnableDefaultReply") {
+    try {
+        Set-ItemProperty -Path "HKLM:SYSTEM\CurrenctControlSet\Control\Error Message Instrument" -Name "EnableDefaultReply" -value 1 
+    } catch {
+        Write-Host "An error occurred while trying to Enable Default Reply" 
+    }
+} else {
+    try {
+        New-ItemProperty -Path "HKLM:SYSTEM\CurrenctControlSet\Control\Error Message Instrument" -Name "EnableDefaultReply" -value 1
+    } catch {
+        Write-Host "An error occurred while trying to (create) Enable Default Reply" 
+    }
 }
