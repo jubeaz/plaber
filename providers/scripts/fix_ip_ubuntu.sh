a35da120f833acaeaadba1cb5f4490f3afe9166d
jubeaz <pascal.auillans@gmail.com>
2024-05-20 16:54:00 +0200

    udate

--- providers/scripts/fix_ip_ubuntu.sh
+++ providers/scripts/fix_ip_ubuntu.sh
@@ -12,7 +12,8 @@ echo "          via: $1"                            >> /etc/netplan/50-vagrant.y
 echo "      nameservers:"                           >> /etc/netplan/50-vagrant.yaml
 echo "          search: [mydomain, otherdomain]"    >> /etc/netplan/50-vagrant.yaml
 echo "          addresses: [8.8.8.8]"               >> /etc/netplan/50-vagrant.yaml
-#sed -i "/Name=en.*/d" /etc/systemd/network/80-dhcp.network
+sed -i "s/true/false/g" /etc/netplan/50-cloud-init.yaml
+netplan apply
 #sed -i "s/Name=eth.*/Name=eth0/g" /etc/systemd/network/80-dhcp.network
 ##echo "[Link]" >> /etc/systemd/network/80-dhcp.network
 ##echo  "Unmanaged=yes" >> /etc/systemd/network/80-dhcp.network
