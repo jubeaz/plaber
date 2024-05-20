3a9175eaeac553629fb9311a6b5844209c5eea1a
jubeaz <pascal.auillans@gmail.com>
2024-05-20 16:42:09 +0200

    update

---
+++ providers/scripts/fix_ip_ubuntu.sh
@@ -0,0 +1,24 @@
+# vmware bug to set the ip
+# see : https://github.com/hashicorp/vagrant/issues/5000#issuecomment-258209286
+
+# eth0 = NAT
+# eth1 = bridge
+# eth 2 = internal
+
+
+echo "      routes:"                                >> /etc/netplan/50-vagrant.yaml
+echo "        - to: default"                        >> /etc/netplan/50-vagrant.yaml
+echo "          via: $1"                            >> /etc/netplan/50-vagrant.yaml
+echo "      nameservers:"                           >> /etc/netplan/50-vagrant.yaml
+echo "          search: [mydomain, otherdomain]"    >> /etc/netplan/50-vagrant.yaml
+echo "          addresses: [8.8.8.8]"               >> /etc/netplan/50-vagrant.yaml
+#sed -i "/Name=en.*/d" /etc/systemd/network/80-dhcp.network
+#sed -i "s/Name=eth.*/Name=eth0/g" /etc/systemd/network/80-dhcp.network
+##echo "[Link]" >> /etc/systemd/network/80-dhcp.network
+##echo  "Unmanaged=yes" >> /etc/systemd/network/80-dhcp.network
+#
+#sed -i "s/24/16/g" /etc/systemd/network/eth1.network
+#echo "[Route]" >>  /etc/systemd/network/eth1.network
+#echo "Gateway=$1" >>  /etc/systemd/network/eth1.network
+#systemctl enable systemd-networkd
+#systemctl restart systemd-networkd
