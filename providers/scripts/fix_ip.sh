562dbd623218831d6dfb8862b3315d40cd78e8ed
jubeaz <pascal.auillans@gmail.com>
2025-02-23 04:38:41 +0100

    update

--- providers/scripts/fix_ip.sh
+++ providers/scripts/fix_ip.sh
@@ -10,8 +10,11 @@ sed -i 's/Name=eth.*/Name=eth0/g' /etc/systemd/network/80-dhcp.network
 #echo '[Link]' >> /etc/systemd/network/80-dhcp.network
 #echo  'Unmanaged=yes' >> /etc/systemd/network/80-dhcp.network
 
-sed -i 's/24/16/g' /etc/systemd/network/eth1.network
-echo "[Route]" >>  /etc/systemd/network/eth1.network
-echo "Gateway=$1" >>  /etc/systemd/network/eth1.network
+if [[ "$1" == "routed" ]]; then
+    echo "Will use routed interface"    
+    sed -i 's/24/16/g' /etc/systemd/network/eth1.network
+    echo "[Route]" >>  /etc/systemd/network/eth1.network
+    echo "Gateway=$2" >>  /etc/systemd/network/eth1.network
+fi
 systemctl enable systemd-networkd
 systemctl restart systemd-networkd
