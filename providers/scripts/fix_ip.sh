bf8a675b7da6a2cd89a55d127c4447f566ef922c
jubeaz <pascal.auillans@gmail.com>
2025-03-20 10:03:28 +0100

    update

--- providers/scripts/fix_ip.sh
+++ providers/scripts/fix_ip.sh
@@ -5,16 +5,18 @@
 # eth1 = bridge
 # eth 2 = internal
 
+echo "Set DHCP only on NAT interface (eth0)"
 sed -i '/Name=en.*/d' /etc/systemd/network/80-dhcp.network
 sed -i 's/Name=eth.*/Name=eth0/g' /etc/systemd/network/80-dhcp.network
 #echo '[Link]' >> /etc/systemd/network/80-dhcp.network
 #echo  'Unmanaged=yes' >> /etc/systemd/network/80-dhcp.network
 
 if [[ "$1" == "routed" ]]; then
-    echo "Will use routed interface"    
+    echo "Will use routed interface (eth1)"    
     sed -i 's/24/16/g' /etc/systemd/network/eth1.network
     echo "[Route]" >>  /etc/systemd/network/eth1.network
     echo "Gateway=$2" >>  /etc/systemd/network/eth1.network
 fi
+echo "Ensure Networkd is started"
 systemctl enable systemd-networkd
 systemctl restart systemd-networkd
