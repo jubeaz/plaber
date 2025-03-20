086580174f7b857cd42872530046ef0b75744a39
jubeaz <pascal.auillans@gmail.com>
2025-03-20 10:18:03 +0100

    fix

--- providers/scripts/arch_python.sh
+++ providers/scripts/arch_python.sh
@@ -3,12 +3,12 @@ pacman -Syu --noconfirm
 pacman -S --noconfirm python reflector
 
 # cp /etc/pacman.d/mirrorlist.pacnew /etc/pacman.d/mirrorlist  
-/usr/bin/reflector --verbose  --country ${COUNTRIES} --latest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+/usr/bin/reflector --verbose  --country $1 --latest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
 echo "" > /etc/xdg/reflector/reflector.conf
 echo "--save /etc/pacman.d/mirrorlist" >> /etc/xdg/reflector/reflector.conf
 echo "--protocol https" >> /etc/xdg/reflector/reflector.conf
-echo "--country France" >> /etc/xdg/reflector/reflector.conf
+echo "--country $1" >> /etc/xdg/reflector/reflector.conf
 echo "--latest 5" >> /etc/xdg/reflector/reflector.conf
 echo "--sort rate" >> /etc/xdg/reflector/reflector.conf
 
