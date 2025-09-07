a729bf4a853650d31a91a064cdd3cb3567fab049
jubeaz <pascal.auillans@gmail.com>
2025-09-07 22:12:07 +0200

    update

--- providers/scripts/flush-interface.ps1
+++ providers/scripts/flush-interface.ps1
@@ -1,3 +1,5 @@
 param ([string]$IFName)
+    Write-HostVerbose "Disable-NetAdapter $IFName"
 	Disable-NetAdapter -Name $IFName -Confirm:$false
+    Write-HostVerbose "Enable-NetAdapter $IFName"
 	Enable-NetAdapter -Name $IFName -Confirm:$false
