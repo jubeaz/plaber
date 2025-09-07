1232d1eda42b60dbd57ae7a7d1273b7579d3e4ba
jubeaz <pascal.auillans@gmail.com>
2025-09-07 22:05:32 +0200

    update

--- providers/scripts/rename-interface.ps1
+++ providers/scripts/rename-interface.ps1
@@ -1,6 +1,8 @@
 param ([string]$IFName, [string]$NetPrefix = $false)
+	Write-HostVerbose "Renaming interface containing $NetPrefix to $IFName"
 	$interface = Get-NetIPAddress | Where-Object { $_.IPAddress -like $NetPrefix }
 	$interfaceName = $interface.InterfaceAlias
+	Write-HostVerbose "Found $interfaceName"
 	Rename-NetAdapter -Name $interfaceName -NewName $IFName
 	#Disable-NetAdapter -Name $IFName -Confirm:$false
 	#Enable-NetAdapter -Name $IFName -Confirm:$false
